9768f84f862b3d2808e370adf420f98f0664f060
Matija Jeras <matija.jeras64@gmail.com>
2022-10-16 02:18:06 +0200

    implementing scrape

--- main.py
+++ main.py
@@ -2,21 +2,29 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import json
 import os
 import time
 
+REGIONS = [
+   'ljubljana-mesto',
+   'ljubljana-okolica',
+   'juzna-primorska',
+   'severna-primorska',
+   'notranjska'
+]
 
-def scrape_top_news():
-
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+def get_items(browser, type):
+    f = open('{type}.json')
+    old = json.load(f)
 
     wait = WebDriverWait(browser, 10)
-    browser.get('https://news.ycombinator.com/')
+
+    url = 'https://www.nepremicnine.net/oglasi-prodaja/{REGIONS[r]}/{type}?s=16'
+    if type == "posest":
+        url = 'https://www.nepremicnine.net/oglasi-prodaja/{REGIONS[r]}/{type}/zazidljiva?s=16'
+    
+    browser.get(url)
     element_list = wait.until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".title > a"))
     )
@@ -26,9 +34,21 @@ def scrape_top_news():
             print("Title:", title, "\nURL:", url, end="\n\n")
         except Exception as e:
             print(e)
+
+def scrape():
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+
+    get_items(browser, "posest")
+
     time.sleep(2)
     browser.quit()
 
-
 if __name__ == '__main__':
-    scrape_top_news()
+    print("Starting scrape...")
+    scrape()
